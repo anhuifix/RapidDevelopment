7222a3cbc3f17b42c71d5bd9e87bc31dc76e52a7
anhuifix <anhuifix@gmail.com>
2017-01-10 13:42:27 +0800

    创建基类

--- app/src/main/java/com/rapid/rapiddevelopment/UI/base/BaseActivity.java
+++ app/src/main/java/com/rapid/rapiddevelopment/UI/base/BaseActivity.java
@@ -1,8 +1,16 @@
 package com.rapid.rapiddevelopment.UI.base;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.FragmentActivity;
+
 /**
  * Created by anhui on 2017-1-6.
  */
 
-public class BaseActivity {
+public class BaseActivity extends FragmentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
--- app/src/main/java/com/rapid/rapiddevelopment/UI/base/BaseFragment.java
+++ app/src/main/java/com/rapid/rapiddevelopment/UI/base/BaseFragment.java
@@ -1,8 +1,11 @@
 package com.rapid.rapiddevelopment.UI.base;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by anhui on 2017-1-6.
  */
 
-public class BaseFragment {
+public class BaseFragment extends Fragment {
+
 }
